abe7d104421be95e405caa2a7adde8c5c214ee2e
Palak <98663148+palak-7@users.noreply.github.com>
2024-07-30 10:49:31 +0530

    removed testimonials

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ const page = () => {
       <VideoSection />
       <Numbers />
       <Offering />
-      <Testimonial />
+      {/* <Testimonial /> */}
       {/* <News /> */}
       <ChairmanWords />
     </div>
